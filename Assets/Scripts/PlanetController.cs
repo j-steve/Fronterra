fb0973544ecec654da5de30e29b7c50b0bd23967
Steve <Steve5805@gmail.com>
2015-08-31 19:01:59 -0400

    created project

---
+++ Assets/Scripts/PlanetController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetController : MonoBehaviour {
+
+	private const float GRAV_CONST = 6.67384E-11f;
+
+
+	public Rigidbody playerShip;
+
+	public GameObject atmosphere;
+
+	public Rigidbody moon;
+
+	public float mass;
+
+	// Use this for initialization
+	void Start () {
+		moon.AddForce (15000, 0, 0);
+		moon.drag = 0;
+		moon.angularDrag = 0;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Rotate(new Vector3(0f, -0.02f, 0f));
+		atmosphere.transform.Rotate(new Vector3(0f, 0.04f, 0f));
+		applyGravity (playerShip);
+		applyGravity (moon);
+//		Debug.Log (gravitationalPull);
+		//playerShip.AddRelativeForce (gravitationalPull);
+		//playerShip.AddForce (positionDiff / 100);
+	}
+
+	private void applyGravity(Rigidbody target) {
+		
+		float radius = Vector3.Distance (target.position, gameObject.transform.position);
+		Vector3 positionDiff = this.gameObject.transform.position - target.position;
+		positionDiff *= mass / Mathf.Pow (radius, 2);
+		target.AddForce (positionDiff);
+	}
+
+	private float gravTransform(float value) {
+		return GRAV_CONST * (mass > 0 ? 1 : -1) / Mathf.Pow (value, 2);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public UnityEngine.UI.Text speedDisplay;
+
+	public Rigidbody rb;
+
+	public AudioSource engineSound;
+
+	public float forwardAcceleration;
+
+	void Start() { 
+	}
+
+	void FixedUpdate () {
+		float acceleration = getModifier (KeyCode.LeftShift, KeyCode.LeftControl) * forwardAcceleration;
+		engineSound.mute = acceleration <= 0;
+		float verticalAccel = getModifier (KeyCode.Space, KeyCode.V);
+		rb.AddRelativeForce(new Vector3(acceleration, 0, verticalAccel));
+		speedDisplay.text = rb.velocity.x.ToString();
+
+		
+		float spin = getModifier (KeyCode.Q, KeyCode.E) / 5;
+		float lateralTilt = getModifier (KeyCode.D, KeyCode.A) / 5;
+		float verticalTilt = getModifier (KeyCode.W, KeyCode.S) / 5; 
+		rb.AddRelativeTorque (spin, verticalTilt, lateralTilt);
+	}
+
+	private float getModifier (KeyCode keyForward, KeyCode keyBackward) {
+		return Input.GetKey (keyForward) ? 1 : Input.GetKey (keyBackward) ? -1 : 0;
+	}
+}
